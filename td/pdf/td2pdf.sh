5ba0e896791ba6543ba44fc4dd4cc226daf2ffa3
Pierre Benard <pierre.g.benard@inria.fr>
2020-06-29 11:02:07 +0200

    Prog : update PDF

--- td/pdf/td2pdf.sh
+++ td/pdf/td2pdf.sh
@@ -1,8 +1,8 @@
 n=1
 while read i; do 
   printf -v j "%04d" $n
-  wkhtmltopdf --page-size A4 --javascript-delay 5000 --margin-top 0.75in --margin-right 0.75in --margin-bottom 0.75in --margin-left 0.75in --encoding UTF-8 "$i" "$j.pdf";
+  wkhtmltopdf --page-size A4 --javascript-delay 10000 --margin-top 0.75in --margin-right 0.75in --margin-bottom 0.75in --margin-left 0.75in --encoding UTF-8 "$i" "$j.pdf";
   n=$((n+1))
 done < url-list.txt
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -sOutputFile=bloc4-td.pdf $(ls -1 *.pdf)
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -sOutputFile=bloc4-td.pdf $(ls -1 0*.pdf)
 rm 0*.pdf
